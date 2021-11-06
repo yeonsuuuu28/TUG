2f2c80d52a6b1495c4154ff869afbae75c7e721f
auejin <hamaj@kaist.ac.kr>
2021-11-07 05:12:21 +0900

    chat room test

--- src/components/chat_room.jsx
+++ src/components/chat_room.jsx
@@ -1,6 +1,86 @@
 import React from 'react'
+import {
+    useRef,
+    useState
+} from 'react';
 
-const chat = () => {
+
+import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  CustomMessage,
+  MessageSeparator,
+  MessageGroup,
+  TypingIndicator
+} from "@chatscope/chat-ui-kit-react";
+
+// for chat ui, we are using https://chatscope.io/demo/
+// https://chatscope.io/storybook/react/?path=/story/components-chatcontainer--live-controlled-example-with-grouped-messages
+
+
+const Chat = () => {
+    
+    
+    const remoteSender = "Fish";
+    const localSender = "You";
+    const groupIdRef = useRef(0);
+    const msgIdRef = useRef(0);
+    const remoteMsgCnt = useRef(0);
+    const inputRef = useRef();
+    const [msgInputValue, setMsgInputValue] = useState("");
+    const [groups, setGroups] = useState([]);
+
+    const handleSend = (message, sender, notCancel) => {
+        if (groups.length > 0) {
+            const lastGroup = groups[groups.length - 1];
+
+            if (lastGroup.sender === sender) {
+            // Add to group
+            const newMessages = [...lastGroup.messages].concat({
+                _id: `m-${++msgIdRef.current}`,
+                message,
+                sender
+            });
+            const newGroup = { ...lastGroup,
+                messages: newMessages
+            };
+            const newGroups = groups.slice(0, -1).concat(newGroup);
+            setGroups(newGroups);
+            } else {
+            // Sender different than last sender - create new group 
+            const newGroup = {
+                _id: `g-${++groupIdRef.current}`,
+                direction: sender === localSender ? "outgoing" : "incoming",
+                messages: [{
+                _id: `m-${++msgIdRef.current}`,
+                message,
+                sender
+                }]
+            };
+            setGroups(groups.concat(newGroup));
+            }
+        } else {
+            const newGroup = {
+            _id: `g-${++groupIdRef.current}`,
+            direction: sender === localSender ? "outgoing" : "incoming",
+            messages: [{
+                _id: `m-${++msgIdRef.current}`,
+                message,
+                sender: sender
+            }]
+            };
+            setGroups([newGroup]);
+        }
+
+        if (!notCancel) {
+            setMsgInputValue("");
+            inputRef.current.focus();
+        }
+    };
 
     function handleclick(){
         window.location.href = "/mypage";
@@ -9,8 +89,30 @@ const chat = () => {
     return (
         <div>
             <button onClick = {handleclick}>MYPAGE TO CHECK!</button>
+            <button border={true} onClick={() => handleSend(`Please be my teammate! I'm telling you ${remoteMsgCnt.current++} times!`, remoteSender, true)} style={{
+            marginBottom: "1em"
+            }}>Add remote message</button>
+            <div style={{ position: "relative", height: "500px" }}> 
+            <MainContainer>
+            <ChatContainer>
+                <MessageList typingIndicator={<TypingIndicator content={`${remoteSender} is typing`}/>}>
+                    {groups.map(g => <MessageGroup key={g._id} data-id={g._id} direction={g.direction}>
+                    <MessageGroup.Header>{remoteSender} one minute ago</MessageGroup.Header>
+                    <MessageGroup.Messages key={g._id}>
+                        {g.messages.map(m => <Message as={Message} key={m._id} data-id={m._id} model={m} />)}
+                    </MessageGroup.Messages>
+                    </MessageGroup>)}
+                </MessageList>
+                <MessageInput 
+                    placeholder="Get to know your teammates!" attachButton={false}
+                    onSend={m => handleSend(m, localSender)}
+                    onChange={setMsgInputValue}
+                    value={msgInputValue} ref={inputRef} />
+            </ChatContainer>
+            </MainContainer>
+        </div>
         </div>
     )
 }
 
-export default chat
+export default Chat

ceb3dfb055d9440398aec002fdd8e50b1f67ef81
auejin <hamaj@kaist.ac.kr>
2021-11-07 19:05:13 +0900

    display sender name for each message

--- src/components/chat_room.jsx
+++ src/components/chat_room.jsx
@@ -20,7 +20,7 @@ import {
 
 // for chat ui, we are using https://chatscope.io/demo/
 // https://chatscope.io/storybook/react/?path=/story/components-chatcontainer--live-controlled-example-with-grouped-messages
-
+// might use this one too https://github.com/chatscope/use-chat, https://github.com/chatscope/use-chat-example
 
 const Chat = () => {
     
@@ -35,33 +35,35 @@ const Chat = () => {
     const [groups, setGroups] = useState([]);
 
     const handleSend = (message, sender, notCancel) => {
+        console.log(sender);
+
         if (groups.length > 0) {
             const lastGroup = groups[groups.length - 1];
 
             if (lastGroup.sender === sender) {
-            // Add to group
-            const newMessages = [...lastGroup.messages].concat({
-                _id: `m-${++msgIdRef.current}`,
-                message,
-                sender
-            });
-            const newGroup = { ...lastGroup,
-                messages: newMessages
-            };
-            const newGroups = groups.slice(0, -1).concat(newGroup);
-            setGroups(newGroups);
+                // Add to group
+                const newMessages = [...lastGroup.messages].concat({
+                    _id: `m-${++msgIdRef.current}`,
+                    message,
+                    sender
+                });
+                const newGroup = { ...lastGroup,
+                    messages: newMessages
+                };
+                const newGroups = groups.slice(0, -1).concat(newGroup);
+                setGroups(newGroups);
             } else {
-            // Sender different than last sender - create new group 
-            const newGroup = {
-                _id: `g-${++groupIdRef.current}`,
-                direction: sender === localSender ? "outgoing" : "incoming",
-                messages: [{
-                _id: `m-${++msgIdRef.current}`,
-                message,
-                sender
-                }]
-            };
-            setGroups(groups.concat(newGroup));
+                // Sender different than last sender - create new group 
+                const newGroup = {
+                    _id: `g-${++groupIdRef.current}`,
+                    direction: sender === localSender ? "outgoing" : "incoming",
+                    messages: [{
+                    _id: `m-${++msgIdRef.current}`,
+                    message,
+                    sender
+                    }]
+                };
+                setGroups(groups.concat(newGroup));
             }
         } else {
             const newGroup = {
@@ -87,7 +89,8 @@ const Chat = () => {
     }
 
     return (
-        <div>
+        <div>            
+            <script src="node_modules/@chatscope/chat-ui-kit-react/dist/chat-ui-kit-react.min.js"></script>
             <button onClick = {handleclick}>MYPAGE TO CHECK!</button>
             <button border={true} onClick={() => handleSend(`Please be my teammate! I'm telling you ${remoteMsgCnt.current++} times!`, remoteSender, true)} style={{
             marginBottom: "1em"
@@ -97,9 +100,12 @@ const Chat = () => {
             <ChatContainer>
                 <MessageList typingIndicator={<TypingIndicator content={`${remoteSender} is typing`}/>}>
                     {groups.map(g => <MessageGroup key={g._id} data-id={g._id} direction={g.direction}>
-                    <MessageGroup.Header>{remoteSender} one minute ago</MessageGroup.Header>
-                    <MessageGroup.Messages key={g._id}>
-                        {g.messages.map(m => <Message as={Message} key={m._id} data-id={m._id} model={m} />)}
+                    <MessageGroup.Header>{`${g.messages[0].sender}`}</MessageGroup.Header>
+                    <MessageGroup.Messages key={g._id} sender={g.sender}>
+                        {
+                        //g.messages.map(m => <CustomMessage as={Message} key={m._id} data-id={m._id} model={m} />)                        
+                        g.messages.map(m => <Message key={m._id} data-id={m._id} model={m}/>)
+                        }
                     </MessageGroup.Messages>
                     </MessageGroup>)}
                 </MessageList>

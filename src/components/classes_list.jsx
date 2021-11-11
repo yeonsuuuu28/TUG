f70a90c80fe948123e7d9eb04107a6a3fc68c8d2
yeonsuuuu28 <yeonsuuuu28@gmail.com>
2021-11-12 02:06:44 +0900

    added classes

---
+++ src/components/classes_list.jsx
@@ -0,0 +1,59 @@
+    const classes = [
+        {
+            "name" : "Introduction to Programming",
+            "professor" : "Jongmoon Baik",
+            "code" : "CS101"
+        }, 
+        {
+            "name" : "Discrete Mathematics",
+            "professor" : "Sungwon Kang",
+            "code" : "CS204"
+        }, 
+        {
+            "name" : "Data Structure",
+            "professor" : "Eun Young Moon",
+            "code" : "CS206"
+        }, 
+        {
+            "name" : "Programming Principles",
+            "professor" : "Jeehoon Kang",
+            "code" : "CS220"
+        }, 
+        {
+            "name" : "System Programming",
+            "professor" : "Jaehyuk Huh",
+            "code" : "CS230"
+        }, 
+        {
+            "name" : "Introduction to Algorithms",
+            "professor" : "Martin ZIEGLER",
+            "code" : "CS300"
+        }, 
+        {
+            "name" : "Computer Organization",
+            "professor" : "Soontae Kim",
+            "code" : "CS311"
+        }, 
+        {
+            "name" : "Programming Language",
+            "professor" : "Sukyoung Ryu",
+            "code" : "CS320"
+        }, 
+        {
+            "name" : "Operating Systems and Lab",
+            "professor" : "Insik Shin",
+            "code" : "CS330"
+        }, 
+        {
+            "name" : "Introduction to Social Computing",
+            "professor" : "Juho Kim",
+            "code" : "CS473"
+        }, 
+        {
+            "name" : "Introduction to Software Engineering",
+            "professor" : "Doo-Hwan Bae",
+            "code" : "CS350"
+        }
+    ]
+
+export default classes

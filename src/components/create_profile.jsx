27b391d878f5cb9d17a23183bfcb2d76717243a4
yeonsuuuu28 <yeonsuuuu28@gmail.com>
2021-11-16 04:58:30 +0900

    working on profile

--- src/components/create_profile.jsx
+++ src/components/create_profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from './navbar.jsx'
 import Chip from '@mui/material/Chip';
 import Autocomplete from '@mui/material/Autocomplete';
@@ -15,13 +15,14 @@ function handleTags(e) {
     console.log(list_value);
 }
 
-const profile = () => {
+export default function Profile() {
 
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
 const top100Films = [
     { title: 'placeholder' },
   ];
-
+  const [open, setOpen] = useState(false);
+  const [inputValue, setInputValue] = useState("");
     return (
         <div>
             <Navbar/>
@@ -29,12 +30,29 @@ const top100Films = [
             <div className="join_title">Create Your Profile</div>
             <div className="tag">
             <Autocomplete
+                open={open}
                 multiple
                 autoComplete="off"
                 freeSolo
                 autoSelect
                 options={top100Films}
                 onChange={handleTags}
+                onOpen={() => {
+                    if (inputValue) {
+                      setOpen(false);
+                    }
+                    else {
+                        setOpen(false)
+                    }
+                  }}
+                onClose={() => setOpen(false)}
+                onInputChange={(e, value, reason) => {
+                    setInputValue(value);
+            
+                    if (!value) {
+                      setOpen(false);
+                    }
+                  }}
                 noOptionsText
                 getOptionLabel={option => option.title || option}
                 filterSelectedOptions
@@ -50,5 +68,3 @@ const top100Films = [
         </div>
     )
 }
-
-export default profile

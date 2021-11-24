9b4cf765c79cff50a55eecbacb6d3cf34ec51003
yeonsuuuu28 <yeonsuuuu28@gmail.com>
2021-11-25 03:01:10 +0900

    css for chatting page

--- src/components/chat_room.jsx
+++ src/components/chat_room.jsx
@@ -207,10 +207,53 @@ const ProfileTags = ({skills, hobbies}) => {
     if (skills.length * hobbies.length > 0) {
         return (
             <div>
-                {<h2>Skill-Sets</h2>}
-                {skills.map(tag => `#${tag}\t`)}
-                {<h2>Interesting Facts</h2>}
-                {hobbies.map(tag => `#${tag}\t`)}
+                {<div style={{
+                    fontFamily: "Lato, sans-serif",
+                    fontSize: "15pt",
+                    fontWeight: "bold",
+                    paddingLeft: "20px",
+                    paddingTop: "10px"
+                }}>
+                    Skill-Sets
+                </div>}
+                <div style={{
+                    fontFamily: "Lato, sans-serif",
+                    paddingTop: "20px",
+                    paddingBottom: "20px",
+                }}>{skills.map(tag => <span style={{
+                    marginLeft: "20px",
+                    backgroundColor: "#b3cbef",
+                    paddingLeft: "15px",
+                    paddingRight: "15px",
+                    paddingBottom: "7px",
+                    paddingTop: "7px",
+                    borderRadius: "15px",
+                    color: "#333333"
+                }}># {tag}</span>)}
+                </div>
+                {<div style={{
+                    fontFamily: "Lato, sans-serif",
+                    fontSize: "15pt",
+                    fontWeight: "bold",
+                    paddingLeft: "20px",
+                    paddingTop: "15px"
+                }}>Interesting Facts</div>}
+                                <div style={{
+                    fontFamily: "Lato, sans-serif",
+                    paddingTop: "20px",
+                    paddingBottom: "20px",
+                }}>
+                {hobbies.map(tag => <span style={{
+                    marginLeft: "20px",
+                    backgroundColor: "#b3cbef",
+                    paddingLeft: "15px",
+                    paddingRight: "15px",
+                    paddingBottom: "7px",
+                    paddingTop: "7px",
+                    borderRadius: "15px",
+                    color: "#333333"
+                }}># {tag}</span>)}
+                </div>
             </div>
         )
     }
@@ -617,18 +660,39 @@ const RealChat = ({ classId, roomId, senderId, senderName, namePairs, chatRound}
                 }
                 {(timerSec>0 || timerMin>0) && 
                     ((plotUserId.length<=0 || plotUserId === remoteId) && 
-                        <h1>Click one of chat bubbles to see profile.</h1>)
+                        <div style={{
+                            textAlign: "center", 
+                            paddingTop: "40px",
+                            fontFamily: "Lato, sans-serif", 
+                            fontSize: "20pt",
+                            fontWeight: "bold",
+                            }}>
+                            Click chat bubbles to see others' profiles.
+                        </div>)
                 }
                 {(timerSec>0 || timerMin>0) && 
                     (plotUserId.length>0 && plotUserId !== remoteId) && 
                     <div>
-                        <h1>{namePairs[plotUserId]}</h1> 
+                    <div style={{
+                        textAlign: "center", 
+                        paddingTop: "40px",
+                        fontFamily: "Lato, sans-serif", 
+                        fontSize: "20pt",
+                        fontWeight: "bold",
+                        }}>{namePairs[plotUserId]}
+                    </div>
                         <ProfileTags skills={skills} hobbies={hobbies} />
                     </div>
                 }
                 {(timerSec>0 || timerMin>0) && 
                     (plotUserId.length>0 && plotUserId !== remoteId) && 
-                    (<h2>Credit History</h2>)
+                    (<div style={{
+                        fontFamily: "Lato, sans-serif",
+                        fontSize: "15pt",
+                        fontWeight: "bold",
+                        paddingLeft: "20px",
+                        paddingTop: "30px",
+                    }}>Credit History</div>)
                 }
                 {(timerSec>0 || timerMin>0) && 
                     (plotUserId.length>0 && plotUserId !== remoteId) && 
